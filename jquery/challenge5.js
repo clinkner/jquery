8f86625a9684230a9921fc4555fd4958ee846ba6
clinkner <44011662+clinkner@users.noreply.github.com>
2019-11-26 11:37:05 -0500

    Add files via upload

--- jquery/challenge5.js
+++ jquery/challenge5.js
@@ -31,58 +31,58 @@ imgs.each(function (){
 // var imgs = document.querySelectorAll("img");
 // var msg = "Hover over an image below."
 // for (let i = 0; i < imgs.length;i++){
-// 	imgs[i].onmouseover = function() {
-// 	 	document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
-// 	 	document.querySelector('#image').innerHTML= imgs[i].alt;
-// 	 }
+//  imgs[i].onmouseover = function() {
+//    document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
+//    document.querySelector('#image').innerHTML= imgs[i].alt;
+//   }
 
-// 	 imgs[i].onfocus = function() {
-// 	 	document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
-// 	 	document.querySelector('#image').innerHTML= imgs[i].alt;
-// 	 }
+//   imgs[i].onfocus = function() {
+//    document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
+//    document.querySelector('#image').innerHTML= imgs[i].alt;
+//   }
 
-// 	 imgs[i].onmouseleave = function() {
-// 	 	document.querySelector('#image').style.backgroundImage="url('')";
-// 	 	document.querySelector('#image').innerHTML= msg;
-// 	 }
+//   imgs[i].onmouseleave = function() {
+//    document.querySelector('#image').style.backgroundImage="url('')";
+//    document.querySelector('#image').innerHTML= msg;
+//   }
 
-// 	 imgs[i].onblur = function() {
-// 	 	document.querySelector('#image').style.backgroundImage="url('')";
-// 	 	document.querySelector('#image').innerHTML= msg;
-// 	 }
+//   imgs[i].onblur = function() {
+//    document.querySelector('#image').style.backgroundImage="url('')";
+//    document.querySelector('#image').innerHTML= msg;
+//   }
 // }
 
 
 // $(document).ready(function(){
-// 	$('#image').on({
-// 	    'mouseover': function(){
-// 	        $('#image').attr('src','https://s3-us-west-2.amazonaws.com/s.cdpn.io/389177/bacon2.JPG');
-// 	    }
-// 	});
+//  $('#image').on({
+//      'mouseover': function(){
+//          $('#image').attr('src','https://s3-us-west-2.amazonaws.com/s.cdpn.io/389177/bacon2.JPG');
+//      }
+//  });
 
 
-// 	$("#image") .mouseover(function () {
-// 		this.src= "url('"+imgs[i].src+"')"
-// 	}).mouseout(function () {
-// 		this.src= "url('')"
-// 	});
+//  $("#image") .mouseover(function () {
+//    this.src= "url('"+imgs[i].src+"')"
+//  }).mouseout(function () {
+//    this.src= "url('')"
+//  });
 // })
 
 
 // $(document).ready(function(){
-// 	$("#image").on({
-// 		"mouseover": function(){
-// 			$('#image').attr("src", imgs[i].src)
-// 		})
+//  $("#image").on({
+//    "mouseover": function(){
+//      $('#image').attr("src", imgs[i].src)
+//    })
 
-// 	$("#image").on({
-// 		"focus": function(){
+//  $("#image").on({
+//    "focus": function(){
 
-// 		}
-// 	})
+//    }
+//  })
 
 
-// 	})
+//  })
 
 
 // $("#image").on({
